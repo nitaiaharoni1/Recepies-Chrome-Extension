42f9297b3e47acaed4c6586af728e3a7fb3dc247
Nitai <nitaiaharoni1@gmail.com>
2019-09-26 00:15:05 +0300

    nodejs

--- public/background.js
+++ public/background.js
@@ -1,10 +1,7 @@
-// background.js
-
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // Send a message to the active tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-    });
-});
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+    if (req.action === "action") {
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.pageAction.show(tabs[0].id)
+        });
+    }
+})
--- public/content.js
+++ public/content.js
@@ -1,1 +1,28 @@
-alert("Hello from your Chrome extension!")
+chrome.runtime.sendMessage({action: "show"})
+let infinite_scrolling = document.getElementsByClassName("infinite-scrolling")[0];
+infinite_scrolling.className = "";
+let col4 = document.getElementsByClassName("col4")[0];
+col4.className = "block inner";
+
+removeByClassName('sidebar');
+removeByClassName('ob-widget');
+removeByClassName('OUTBRAIN');
+removeByClassName('rel-scroller-container');
+removeByClassName('banner');
+removeByClassName('related-items');
+removeByClassName('related-all-items');
+
+let sendBtn = document.getElementsByClassName('send-list-mail')[0];
+sendBtn.className = "send-all";
+sendBtn.innerText = "Add all";
+let newBtn = document.createElement('button');
+newBtn.innerHTML = "Add checked";
+newBtn.className = "send-all";
+sendBtn.insertAdjacentElement("afterend", newBtn);
+
+function removeByClassName(className) {
+    let elements = document.getElementsByClassName(className);
+    for (let elem of elements) {
+        elem.parentNode.removeChild(elem)
+    }
+}
