4595f9a3bbd9c98d777fa9d98378bfe7919ac74e
I355539 <nitai.aharoni@sap.com>
2019-09-26 14:32:20 +0300

    test

--- public/background.js
+++ public/background.js
@@ -1,7 +1,33 @@
 chrome.runtime.onMessage.addListener((req, sender, res) => {
-    if (req.action === "action") {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            chrome.pageAction.show(tabs[0].id)
-        });
+    if (req.action === "show") {
+        chrome.pageAction.show(sender.tab.id);
     }
-})
+    if (req.action === "addToJul") {
+        let ingredientJul = req.data;
+        postAllItemsToJul(ingredientJul);
+    }
+});
+
+async function postItemToJulCart(item) {
+    await fetch("https://sap.jul.co.il/?wc-ajax=add_to_cart", {
+        "credentials": "include",
+        "headers": {
+            "accept": "application/json, text/javascript, */*; q=0.01",
+            "accept-language": "en-US,en;q=0.9,he;q=0.8",
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        },
+        "body": `quantity=1&product_sku=teva_shuk_61&product_id=${item}`,
+        "method": "POST",
+        "mode": "no-cors"
+    }).then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    });
+}
+
+async function postAllItemsToJul(ingredientJul) {
+    for (i in ingredientJul) {
+        await postItemToJulCart(ingredientJul[i]);
+    }
+}
--- public/content.js
+++ public/content.js
@@ -1,28 +1,99 @@
+const ingredientJul = [],
+    julJson = {
+        "עגבניות": "148401",
+        "בצלים ירוקים": "148319"
+    },
+    classArray = ['sidebar', 'popup-overlay', 'ob-widget', 'OUTBRAIN', 'rel-scroller-container', 'banner', 'related-items', 'related-all-items', 'ingredients-gallery-switch', 'switch-gallery', 'ingredients-create-cart', 'social-share', 'switch-bottom-btn', 'recipe-tags', 'social-bottom-ending']
+
 chrome.runtime.sendMessage({action: "show"})
-let infinite_scrolling = document.getElementsByClassName("infinite-scrolling")[0];
-infinite_scrolling.className = "";
-let col4 = document.getElementsByClassName("col4")[0];
-col4.className = "block inner";
+removeInfinateScroll();
+changeStyles();
+removeAllClassNames(classArray);
+addCartButton();
+clickAllIngrediensBtn();
 
-removeByClassName('sidebar');
-removeByClassName('ob-widget');
-removeByClassName('OUTBRAIN');
-removeByClassName('rel-scroller-container');
-removeByClassName('banner');
-removeByClassName('related-items');
-removeByClassName('related-all-items');
+let ingredients = getIngrediants();
+for (i in ingredients) {
+    if (julJson[ingredients[i]]) {
+        ingredientJul.push(julJson[ingredients[i]]);
+    }
+}
+chrome.runtime.sendMessage({action: "addToJul", data: ingredientJul})
 
-let sendBtn = document.getElementsByClassName('send-list-mail')[0];
-sendBtn.className = "send-all";
-sendBtn.innerText = "Add all";
-let newBtn = document.createElement('button');
-newBtn.innerHTML = "Add checked";
-newBtn.className = "send-all";
-sendBtn.insertAdjacentElement("afterend", newBtn);
+function getIngrediants() {
+    let ingredientsText = [];
+    let ingredients = document.getElementsByClassName('ingridients')[0];
+    let ingredientSingles = ingredients.getElementsByClassName('ingredient-single');
+    for (i in ingredientSingles) {
+        let ingredient = ingredientSingles[i];
+        let text = ingredient.innerText;
+        console.log(text);
+        ingredientsText.push(text);
+    }
+    return ingredientsText;
+}
+
+function removeAllClassNames(classArray) {
+    for (let className of classArray) {
+        removeByClassName(className);
+    }
+}
 
 function removeByClassName(className) {
-    let elements = document.getElementsByClassName(className);
-    for (let elem of elements) {
-        elem.parentNode.removeChild(elem)
+    try {
+        let elements = document.getElementsByClassName(className);
+        for (let elem of elements) {
+            elem.parentNode.removeChild(elem)
+        }
+    } catch (e) {
+        console.log(`Can't find element with class: ${className}`)
+    }
+}
+
+function changeStyles() {
+    try {
+        let col4 = document.getElementsByClassName("col4")[0];
+        col4.className = "block inner";
+    } catch (e) {
+        console.log(`Can't change style`)
+    }
+}
+
+function clickAllIngrediensBtn() {
+    try {
+        let allIngredientsBtn = document.getElementsByClassName('show-all-ingredients')[0];
+        allIngredientsBtn.click();
+    } catch (e) {
+        console.log(`Can't click allIngredientsBtn`)
+    }
+}
+
+function removeInfinateScroll() {
+    try {
+        let infinite_scrolling = document.getElementsByClassName("infinite-scrolling")[0];
+        infinite_scrolling.className = "";
+    } catch (e) {
+        console.log(`Can't find infinite-scrolling`)
+    }
+}
+
+function addCartButton() {
+    try {
+        let sendBtn = document.getElementsByClassName('send-list-mail')[0];
+
+        let newCheckAllBtn = document.createElement('button');
+        newCheckAllBtn.innerText = "סמן את כל המצרכים";
+        newCheckAllBtn.className = "send-all";
+
+        let newSendCheckedBtn = document.createElement('button');
+        newSendCheckedBtn.innerHTML = "שלח את המצרכים המסומנים ל-Jul";
+        newSendCheckedBtn.className = "send-all";
+        newSendCheckedBtn.style.backgroundColor = "rgb(37,207,230)";
+        sendBtn.insertAdjacentElement("afterend", newSendCheckedBtn);
+        sendBtn.insertAdjacentElement("afterend", newCheckAllBtn);
+
+        sendBtn.parentNode.removeChild(sendBtn)
+    } catch (e) {
+        console.log(`Can't add cart button`)
     }
 }
